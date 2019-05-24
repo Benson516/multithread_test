65b074045f7d0710f14ae298e577c136b8bc25e1
Chun-Feng Huang <benson516@hotmail.com>
2019-05-24 23:46:18 +0800

    Add some basic structures and containers

--- include/ROS_interface/ROS_interface.cpp
+++ include/ROS_interface/ROS_interface.cpp
@@ -1,8 +1,69 @@
 #include <ROS_interface.hpp>
 
+using std::vector;
+using std::string;
 
-ROS_INTERFACE::ROS_INTERFACE(){
+// Constructors
+ROS_INTERFACE::ROS_INTERFACE()
+{
     _num_topics = 0;
+
+
+}
+
+// Setting up topics
+bool load_topics(
+    const vector<string> &topic_name_list_in,
+    const vector<int> &topic_type_list_in,
+    const vector<bool> &topic_is_input_list_in,
+    const vector<size_t> &topic_buffer_length_list_in)
+{
+    // Filling the dataset inside the object
+    // Note: Do not subscribe/advertise topic now
+
+    // Saving the parameters
+    _topic_name_list = topic_name_list_in;
+    _topic_type_list = topic_type_list_in;
+    _topic_is_input_list = topic_is_input_list_in;
+    _topic_buffer_length_list = topic_buffer_length_list_in;
+
+
+    // Parsing parameters
+    //----------------------------//
+    _num_topics = topic_name_list_in.size();
+    
+    // get topic_type_id
+
+    // generate SPSC buffers
+
+
+    //----------------------------//
+
+    return true;
+
+}
+
+
+// Really start the ROS thread
+bool ROS_INTERFACE::start(){
+
+    // Start the ROS thread, really starting the ROS
+    _thread_list.push_back( std::thread(_ROS_worker) );
+}
+
+// Private methods
+//---------------------------------------------//
+void ROS_INTERFACE::_ROS_worker(){
+    // This thread is called by start()
+
+    // Subscribing topics
+
+    // Advertising topics
+
+
+
+    // Start spinning and loop to the end
+
 
 
 }
--- include/ROS_interface/ROS_interface.hpp
+++ include/ROS_interface/ROS_interface.hpp
@@ -8,49 +8,146 @@
 // all_header.h
 #include <all_header.h>
 
-using std::vector;
-using std::string;
 
 
+// Example:
+// Note: the following thing should be done in main code
+// The identifier for message global id
+//-----------------------------------------//
+// List the topic tokens in sequence wuth the same order as the topic names in topics name list
+/*
+enum class MSG_ID{
+    chatter_0,
+    chatter_1,
+    chatter_2,
+    chatter_3,
+    chatter_4,
+    chatter_5
+};
 
-class ROS_INTERFACE{
-public:
+vector<string> topic_name_list;
+vector<int> topic_type_list;
+vector<bool> topic_is_input_list;
+vector<size_t> topic_buffer_length_list;
 
-    // The identifier for msg types
+// Start adding
+topic_name_list.push_back("/chatter_0");
+topic_type_list.push_back(ROS_INTERFACE::String);
+topic_is_input_list.push_back(true);
+topic_buffer_length_list.push_back(10);
+
+topic_name_list.push_back("/chatter_1");
+topic_type_list.push_back(ROS_INTERFACE::String);
+topic_is_input_list.push_back(true);
+topic_buffer_length_list.push_back(10);
+
+topic_name_list.push_back("/chatter_2");
+topic_type_list.push_back(ROS_INTERFACE::String);
+topic_is_input_list.push_back(true);
+topic_buffer_length_list.push_back(10);
+*/
+//-----------------------------------------//
+
+
+// library begins
+//--------------------------------------------------//
+namespace ROS_INTERFACE{
+    // The identifier for message types
+    // Note: the type of the enum is defaultly int
     enum class MSG_TYPE{
         String,
         Image,
         // PointCloud
     };
+}// end of the namespace ROS_INTERFACE
+
+
+
+class ROS_INTERFACE{
+    // "using"
+    using std::vector;
+    using std::string;
+
+public:
 
 
     // Constructors
     ROS_INTERFACE();
+    // Setting up topics
+    bool load_topics(
+        const vector<string> &topic_name_list_in,
+        const vector<int> &topic_type_list_in,
+        const vector<bool> &topic_is_input_list_in,
+        const vector<size_t> &topic_buffer_length_list_in);
+    // Really start the ROS thread
+    bool start();
 
-    // Methods
+    // Getting methods for each type of message
+    // The topic_id should be the "global id"
+    //---------------------------------------------------------//
+    bool get_string(const int topic_id, string & content_out);
+    //---------------------------------------------------------//
+
+    // Sending methods for each type of message
+    // The topic_id should be the "global id"
+    //---------------------------------------------------------//
+    bool send_string(const int topic_id, const string &content_in);
+    //---------------------------------------------------------//
 
 private:
 
     size_t _num_topics;
 
+    // Although we only use "one" thread, by using this container,
+    // we can start the thread later by push_back element
+    vector<std::thread> _thread_list;
+    void _ROS_worker();
 
-    // ROS subscribers
+    // List of topics parameters
+    // TODO: make a containner to contain the following things in a single object for each topic
+    //------------------------------//
+    // Note: the following are setting be user
+    vector<string> _topic_name_list; // mapping - topic_id : topic_name (string)
+    vector<int> _topic_type_list;    // mapping - topic_id : topic_type (enum)
+    vector<bool> _topic_is_input_list; // mapping - topic_id : is_input (bool)
+    vector<size_t> _topic_buffer_length_list; // mapping - topic_id : buffer_length (size_t)
+    //------------------------------//
+
+
+    // List of topic tid in each type of message
+    // Note: this "tid" or type_id is type specified, different from global topic_id
+    //------------------------------//
+    vector<int> _topic_tid_list; // mapping - topic_id : topic_type_id
+    //------------------------------//
+
+
+
+
+    // ROS image transportor (similar to  node handle, but for images)
     image_transport::ImageTransport _ros_it;
 
-    // Buffers
+
+    // Subscribers
+
+    // Publishers
+
+
+    // SPSC Buffers
+    //---------------------------------------------------------//
     // Note: each message type got an array of SPSC buffers with that type,
     //       each single topic use an unique SPSC buffer.
     //       The result is that we will have multiple arrays of SPSC buffers
     //---------------------------------------------------------//
     // String
-
+    vector< async_buffer<string> > buffer_list_string;
     // Image
 
     // PointCloud
 
     //---------------------------------------------------------//
 
-};
+}; // end of the class ROS_INTERFACE
+
 
 
 
