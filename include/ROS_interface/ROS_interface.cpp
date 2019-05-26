44c295b1ef30bf3433ce32ad606250ba5eb8dc68
Chun-Feng Huang <benson516@hotmail.com>
2019-05-27 03:32:44 +0800

    Modify the containers for topics

--- include/ROS_interface/ROS_interface.cpp
+++ include/ROS_interface/ROS_interface.cpp
@@ -4,51 +4,59 @@ using std::vector;
 using std::string;
 
 // Constructors
-ROS_INTERFACE::ROS_INTERFACE()
-{
-    _num_topics = 0;
+ROS_INTERFACE::ROS_INTERFACE():
+    is_started(false),
+    _num_topics(0),
+    _msg_type_2_topic_params(ROS_INTERFACE::MSG_TYPE::NUM_MAG_TYPE)
+{}
 
 
-}
 
 // Setting up topics
-bool load_topics(
-    const vector<string> &topic_name_list_in,
-    const vector<int> &topic_type_list_in,
-    const vector<bool> &topic_is_input_list_in,
-    const vector<size_t> &topic_buffer_length_list_in)
-{
-    // Filling the dataset inside the object
-    // Note: Do not subscribe/advertise topic now
-
-    // Saving the parameters
-    _topic_name_list = topic_name_list_in;
-    _topic_type_list = topic_type_list_in;
-    _topic_is_input_list = topic_is_input_list_in;
-    _topic_buffer_length_list = topic_buffer_length_list_in;
-
-
+//----------------------------------------------------------------//
+// Method 1: use add_a_topic to add a single topic one at a time
+bool add_a_topic(const string &name_in, int type_in, bool is_input_in, size_t buffer_length_in){
+    _topic_param_list.push_back( TOPIC_PARAMS(name_in, type_in, is_input_in, buffer_length_in) );
     // Parsing parameters
     //----------------------------//
-    _num_topics = topic_name_list_in.size();
-    
-    // get topic_type_id
-
-    // generate SPSC buffers
-
-
+    _num_topics = _topic_param_list.size();
+    // get topic_type_id and store them in separated arrays
+    size_t i = _num_topics-1;
+    _msg_type_2_topic_params[ _topic_param_list[i].type ].push_back( _topic_param_list[i] );
+    _topic_tid_list[i] = ( _msg_type_2_topic_params[ _topic_param_list[i].type ].size() - 1 );
     //----------------------------//
-
     return true;
-
 }
-
+// Method 2: use load_topics to load all topics
+bool load_topics(const vector<ROS_INTERFACE::TOPIC_PARAMS> &topic_param_list_in){
+    // Filling the dataset inside the object
+    // Note: Do not subscribe/advertise topic now
+    //----------------------------//
+    // Saving the parameters
+    _topic_param_list = topic_param_list_in;
+    // Parsing parameters
+    //----------------------------//
+    _num_topics = _topic_param_list.size();
+    // get topic_type_id and store them in separated arrays
+    for(size_t i=0; i < _topic_param_list.size(); ++i){
+        _msg_type_2_topic_params[ _topic_param_list[i].type ].push_back( _topic_param_list[i] );
+        _topic_tid_list[i] = ( _msg_type_2_topic_params[ _topic_param_list[i].type ].size() - 1 );
+    }
+    //----------------------------//
+    return true;
+}
+//----------------------------------------------------------------//
 
 // Really start the ROS thread
 bool ROS_INTERFACE::start(){
-
+    if (is_started) return false;
     // Start the ROS thread, really starting the ROS
     _thread_list.push_back( std::thread(_ROS_worker) );
+    is_started = true;
+    return true;
+}
+bool ROS_INTERFACE::is_started(){
+    return is_started;
 }
 
 // Private methods
@@ -56,10 +64,11 @@ bool ROS_INTERFACE::start(){
 void ROS_INTERFACE::_ROS_worker(){
     // This thread is called by start()
 
-    // Subscribing topics
+    // Subscribing topics: generate _subs_id_list, generate SPSC buffers, subscribe
+    // Note: the order is important, since that the callback function should be exposed only when all the variables are set
 
-    // Advertising topics
-
+    // Advertising topics: generated _pub_id_list, generate SPSC buffers, advertise
+    // Note: the order is important, since that the callback function should be exposed only when all the variables are set
 
 
     // Start spinning and loop to the end
--- include/ROS_interface/ROS_interface.hpp
+++ include/ROS_interface/ROS_interface.hpp
@@ -25,26 +25,26 @@ enum class MSG_ID{
     chatter_5
 };
 
-vector<string> topic_name_list;
-vector<int> topic_type_list;
-vector<bool> topic_is_input_list;
-vector<size_t> topic_buffer_length_list;
+ROS_INTERFACE ros_interface;
+// Method 1:
+{
+    using ROS_INTERFACE::MSG_TYPE;
+    ros_interface.add_a_topic("/chatter_0", MSG_TYPE::String, true, 10);
+    ros_interface.add_a_topic("/chatter_1", MSG_TYPE::String, true, 10);
+    ros_interface.add_a_topic("/chatter_2", MSG_TYPE::String, true, 10);
+}
 
-// Start adding
-topic_name_list.push_back("/chatter_0");
-topic_type_list.push_back(ROS_INTERFACE::String);
-topic_is_input_list.push_back(true);
-topic_buffer_length_list.push_back(10);
-
-topic_name_list.push_back("/chatter_1");
-topic_type_list.push_back(ROS_INTERFACE::String);
-topic_is_input_list.push_back(true);
-topic_buffer_length_list.push_back(10);
-
-topic_name_list.push_back("/chatter_2");
-topic_type_list.push_back(ROS_INTERFACE::String);
-topic_is_input_list.push_back(true);
-topic_buffer_length_list.push_back(10);
+// Method 2:
+vector<ROS_INTERFACE::TOPIC_PARAMS> topic_param_list;
+{
+    using ROS_INTERFACE::TOPIC_PARAMS;
+    using ROS_INTERFACE::MSG_TYPE;
+    // Start adding topics
+    topic_param_list.push_back( TOPIC_PARAMS("/chatter_0", MSG_TYPE::String, true, 10) );
+    topic_param_list.push_back( TOPIC_PARAMS("/chatter_1", MSG_TYPE::String, true, 10) );
+    topic_param_list.push_back( TOPIC_PARAMS("/chatter_2", MSG_TYPE::String, true, 10) );
+}
+ros_interface.load_topics(topic_param_list);
 */
 //-----------------------------------------//
 
@@ -58,6 +58,20 @@ namespace ROS_INTERFACE{
         String,
         Image,
         // PointCloud
+        NUM_MAG_TYPE
+    };
+
+    struct TOPIC_PARAMS{
+        std::string name;
+        int type; // According to the enum value defined in ROS_INTERFACE class
+        bool is_input; // if not, it's output
+        size_t buffer_length; // The buffer length setting for the SPSC buffer used for this topic
+        //
+        TOPIC_PARAMS(): type(0), is_input(false), buffer_length(1)
+        {}
+        TOPIC_PARAMS(const std::string &name_in, int type_in, bool is_input_in, size_t buffer_length_in):
+            name(name_in), type(type_in), is_input(is_input_in), buffer_length(buffer_length_in)
+        {}
     };
 }// end of the namespace ROS_INTERFACE
 
@@ -74,13 +88,15 @@ public:
     // Constructors
     ROS_INTERFACE();
     // Setting up topics
-    bool load_topics(
-        const vector<string> &topic_name_list_in,
-        const vector<int> &topic_type_list_in,
-        const vector<bool> &topic_is_input_list_in,
-        const vector<size_t> &topic_buffer_length_list_in);
+    // Method 1: use add_a_topic to add a single topic one at a time
+    // Method 2: use load_topics to load all topics
+    bool add_a_topic(const string &name_in, int type_in, bool is_input_in, size_t buffer_length_in);
+    bool load_topics(const vector<ROS_INTERFACE::TOPIC_PARAMS> &topic_param_list_in);
     // Really start the ROS thread
     bool start();
+
+    // Check if the ROS is started
+    bool is_started();
 
     // Getting methods for each type of message
     // The topic_id should be the "global id"
@@ -95,7 +111,7 @@ public:
     //---------------------------------------------------------//
 
 private:
-
+    bool is_started;
     size_t _num_topics;
 
     // Although we only use "one" thread, by using this container,
@@ -107,23 +123,31 @@ private:
     // TODO: make a containner to contain the following things in a single object for each topic
     //------------------------------//
     // Note: the following are setting be user
-    vector<string> _topic_name_list; // mapping - topic_id : topic_name (string)
-    vector<int> _topic_type_list;    // mapping - topic_id : topic_type (enum)
-    vector<bool> _topic_is_input_list; // mapping - topic_id : is_input (bool)
-    vector<size_t> _topic_buffer_length_list; // mapping - topic_id : buffer_length (size_t)
+    // mapping - topic_id : topic parameters (ROS_INTERFACE::TOPIC_PARAMS)
+    vector<ROS_INTERFACE::TOPIC_PARAMS> _topic_param_list;
     //------------------------------//
-
 
     // List of topic tid in each type of message
-    // Note: this "tid" or type_id is type specified, different from global topic_id
+    // The tid is mainly used for indexing SPSC buffers
+    // Note 1: this "tid" or type_id is type specified, different from global topic_id
+    // Note 2: These vetor will be filled at adding/loading topics
     //------------------------------//
     vector<int> _topic_tid_list; // mapping - topic_id : topic_type_id
+    // Topic-param list for each message type
+    vector< vector<ROS_INTERFACE::TOPIC_PARAMS> > _msg_type_2_topic_params; // _msg_type_2_topic_params[type][tid] --> TOPIC_PARAMS
+    //------------------------------//
+
+    // The subs_id and pub_id for each topic_id
+    // The _subs_id/_pub_id is mainly used for indexing subscribers/publishers
+    // Note: these ids as well as SPSC buffers will be generated when subscribing/advertising
+    //------------------------------//
+    vector<int> _subs_id_list;
+    vector<int> _pub_id_list;
     //------------------------------//
 
 
 
-
-    // ROS image transportor (similar to  node handle, but for images)
+    // ROS image transport (similar to  node handle, but for images)
     image_transport::ImageTransport _ros_it;
 
 
